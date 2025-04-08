68331c06230fce05b34ffa43ef87c05bfd1caa86
SanRaph <raphytex@gmail.com>
2025-04-08 07:11:54 -0700

    start with login

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+import './Login.css';
+import React from 'react';
+import { useState } from 'react';
+
+const API_URI = window.location.host === 'localhost' ? 'http://localhost:3000/users/login' : '';
+
+
+const Login = () => {
+    
+};
