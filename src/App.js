3462e7b033a1b3e555184bd68f469cf16da4af02
SanRaph <raphytex@gmail.com>
2025-04-09 06:17:04 -0700

    creating routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './Registration';
 import './App.css';
 import Login from './Login'
 import Registration from './Registration';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (<>
-  <Login />
-  <Registration />
-  <h1 className='App'>Happy Hacking!</h1>
-  </>
+  return (
+    <Router>
+        <div>
+        <h1>React Router Example</h1>
+        <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*"  />
+        </Routes>
+      </div>
+
+
+    </Router>
   );
 }
 
