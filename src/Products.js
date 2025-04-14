079c8b9dfc5efe99333d47c99156eb04bb35e052
SanRaph <raphytex@gmail.com>
2025-04-14 03:15:39 -0700

    working on product card

--- src/Products.js
+++ src/Products.js
@@ -31,18 +31,59 @@ function ProductPage() {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div className="product-page">
-      <div className="product-grid">
-        {products.map(product => (
-          <div className="product-card" key={product.id}>
-            <img src={product.image} alt={product.title} width="100" />
-            <small>{product.title}</small>
-            <p>${product.price}</p>
-            <Button>Add to Cart</Button>
-          </div>
+    <div >
+        {products.length > 0 && products.map((product) => (
+          
+          <section class="product" key={product.id}>
+	           <div class="product__photo">
+		               <div class="photo-container">
+			                <div class="photo-main">
+			                 	<div class="controls">
+				                	<i class="material-icons">share</i>
+				                	<i class="material-icons">favorite_border</i>
+			              	</div>
+			               <img src={product.image} alt={product.title} />
+			            </div>
+			   <div class="photo-album">
+				<ul>
+					<li><img src={product.image} alt="green apple" /></li>
+					<li><img src={product.image} alt="half apple" /></li>
+					<li><img src={product.image} alt="green apple" /></li>
+					<li><img src={product.image} alt="apple top" /></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="product__info">
+		<div class="title">
+			<h1>{product.title}</h1>
+			<span>COD: 45999</span>
+		</div>
+		<div class="price">&#8358;
+			 <span>{product.price}</span>
+		</div>
+		<div class="variant">
+			<h3>SELECT A COLOR</h3>
+			<ul>
+				<li><img src={product.image} alt="green apple" /></li>
+				<li><img src={product.image} alt="yellow apple" /></li>
+				<li><img src={product.image} alt="orange apple" /></li>
+				<li><img src={product.image} alt="red apple" /></li>
+			</ul>
+		</div>
+		<div class="description">
+			<h3>BENEFITS</h3>
+			<ul>
+				<li>{product.decription}</li>
+			</ul>
+		</div>
+		<button class="buy--btn">ADD TO CART</button>
+	</div>
+</section>
+
         ))}
       </div>
-    </div>
+
   );
 }
 
